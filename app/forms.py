aa9609f8299a169496acc82d55dd6f98c48b3de6
ksg-dev <ksg.dev.data@gmail.com>
2024-08-09 16:08:25 -0500

    edit rating and review functionality

---
+++ app/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DecimalField, SubmitField, HiddenField
+from wtforms.validators import InputRequired, NumberRange, ValidationError
+from app import db
+from app.models import Movie
+
+class RateMovieForm(FlaskForm):
+    rating = DecimalField('Rating',
+                          validators=[
+                              InputRequired(),
+                              NumberRange(min=0, max=10, message="Please enter a rating between 0.0 and 10.0")
+                          ])
+    review = StringField('Review',
+                         validators=[
+                             InputRequired()
+                         ])
+    submit = SubmitField("Update Movie")
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from app import app, db
-
+from app.forms import RateMovieForm
 from app.models import Movie, validate_title
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap5
 
 bootstrap = Bootstrap5(app)
+
 
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -16,3 +17,17 @@ def home():
 
     else:
         return render_template('index.html', library=Movie.query.all(), query=None)
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("edit.html", movie=movie, form=form)
